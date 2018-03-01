2258fd6ef930de7b0ecbe692702149f4ee9028fe
Leo D. Nuneza <leiyu876@yahoo.com>
2018-03-01 18:25:45 +0300

    fix helper issue

--- app/config/autoload.php
+++ app/config/autoload.php
@@ -86,7 +86,7 @@ $autoload['drivers'] = array();
 |   $autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url', 'form', 'cookie', 'language', 'file', 'date', 'datatable');
+$autoload['helper'] = array('url', 'form', 'cookie', 'language', 'file', 'date');
 
 
 /*
