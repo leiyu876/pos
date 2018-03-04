95b168ec05890438ef419ed842fd8b433bb5813b
Leo Nuneza <leiyu876@yahoo.com>
2018-03-05 00:56:26 +0300

    remove custom email templates because of the server setup

--- app/models/admin/Customemail_model.php
+++ app/models/admin/Customemail_model.php
@@ -16,14 +16,34 @@ class Customemail_Model extends CI_Model
         $contact_detail = $this->site->getUser($action_to);
         $groups = $this->ion_auth->getUserGroup($action_to);
         
-        $v = file_get_contents('./themes/' . $this->theme . 'email_templates/custom/'.$action.'.html');
+        $v = $this->email_view();
 
         $v = str_replace("{site_name}", $this->Settings->site_name, $v);
         $v = str_replace("{product_name}", $product_detail->name, $v);
         $v = str_replace("{product_code}", $product_detail->code, $v);
         $v = str_replace("{contact_person}", $contact_detail->first_name.' '.$contact_detail->last_name, $v);
         $v = str_replace("{company}", $groups->description, $v);
+        $v = str_replace("{action}", $action, $v);
 
         $this->sma->send_email($contact_detail->email, 'Borrowed Product Details', $v);
+    } 
+
+    public function email_view() {
+
+        return '
+        <h3>{logo}</h3>
+        <h4>Borrowed Product Details</h4>
+        <p>
+            Hello {contact_person} ({company}),
+        </p>
+        <p>
+            Product({product_name}) with code({product_code}) was {action} to you. <br/>
+            Please contact Admin to manage your borrowed products.
+        </p>
+        <p>
+            Best regards,
+            <br>
+            {site_name}
+        </p>';
     }
 }
